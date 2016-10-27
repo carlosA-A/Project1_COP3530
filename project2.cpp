467da046636eef41d2cd188169f6fc2068076003
Ubuntu2 <c.avogadro@ufl.edu>
2016-10-27 12:27:03 -0400

    Fixed nested functions

--- project2.cpp
+++ project2.cpp
@@ -5,9 +5,6 @@
 #include <stack>
 #include <math.h> 
 #include <string>
-//Defining constants
-#define Pi 3.14169
-#define e  2.718
 
 using namespace std;
 
@@ -30,14 +27,14 @@ int main ()
 
     while (calc != "exit")
     {
-        
+
         getline (std::cin,calc);
 
-            
+
         //shantin_yard(calc);
         check_var(calc);
-    
-    
+
+
     }
 
     return 0;
@@ -89,27 +86,27 @@ double shantin_yard(string expression)
                     operators.push('l');
 
 
-                
-            
+
+
             }
             //Value is a variable so retrieve value
             else
             {
-                
+
                 var_val = get_var(var_name);
                 if (var_val == -1)
                 {
-                
+
                     return -1;
                 }
                 else
                 {
                     result.push(to_string(var_val));   
-                
+
                 }
 
-        
-                
+
+
             }
         }
 
@@ -128,19 +125,19 @@ double shantin_yard(string expression)
 
                 if(expression[i] == '.')
                 {
-                
-                num += expression[i];
-                i++;
+
+                    num += expression[i];
+                    i++;
                 }
-            
+
             }
             result.push(num);//Push number into queue after it has been read
-       
+
         }
 
         if (expression[i] == '(' ||expression[i] == ')' ||expression[i] == '+' || expression[i] == '-' ||expression[i] == '*' ||expression[i] == '/' ||expression[i] == '^' )//Check for operators
         {
-        
+
             cout<<"expression executes"<<endl;
             if (operators.empty()) //If stack with operators is empty then push operator into stack, no need to compare
             {
@@ -155,57 +152,68 @@ double shantin_yard(string expression)
                 {
                     cout<<"pushing ( to stack"<<endl;
                     operators.push(expression[i]);
-                
+
                 }
                 else if (expression[i] == ')')
                 {
                     cout<<"pushing operators to stack"<<endl;
                     //Handle the order of operations when we see a parenthesis
-                   while(operators.top() != '(')
+                    while(operators.top() != '(')
                     {
-                               
-                        
-                        //Push operator to result queue, pop it from stack and add new operator to stack
-                        result.push(to_string(add_nums(result,operators.top())));
-                        operators.pop();
+
+
+                        if(operators.top() == 's' ||operators.top() == 'c' ||operators.top() == 'l' )
+                        {
+                            cout<<"sin eval"<<endl;
+                            result.push(to_string(eval_func(result,operators.top())));
+                            operators.pop();
+
+                        }
+                        else
+                        {
+
+                            cout<<"calculation eval"<<endl;
+                            result.push(to_string(add_nums(result,operators.top())));
+                            operators.pop();
+                        }
                     }
                     operators.pop();//Remove the ( so that it doesn't stay in our result
-                
+
                 }
                 else
-                
+
                 {
                     cout<<"Check precedence "<<endl;
                     while(!operators.empty() && precedence.at(operators.top()) >= precedence.at(expression[i]))
-                    
+
                     {
                         if(operators.top() == 's' ||operators.top() == 'c' ||operators.top() == 'l' )
                         {
                             cout<<"sin eval"<<endl;
                             result.push(to_string(eval_func(result,operators.top())));
                             operators.pop();
-                        
+
                         }
                         else
                         {
-                        
+
                             cout<<"calculation eval"<<endl;
                             result.push(to_string(add_nums(result,operators.top())));
                             operators.pop();
                         }
 
-                    
+
                     }
 
                     operators.push(expression[i]); // push in new expression
                 }
             }
-        
+
         }
 
-        
-    
-    
+
+
+
     }
     //push all remaining elements in the stack to our result queue 
 
@@ -225,14 +233,14 @@ double shantin_yard(string expression)
             result.push(to_string(add_nums(result,operators.top())));
             operators.pop();
         }
-    
+
     }
-     //cout<<result.top()<<endl;
+    //cout<<result.top()<<endl;
     double final_result = stod(result.top());
     cout<< final_result;
     return final_result;
     //result.pop();
-    
+
 
 }
 
@@ -242,25 +250,25 @@ double add_nums (stack <string> &result,char ch)
     result.pop();
     double temp_num2 = stod(result.top());
     result.pop();
-    
+
     if(ch == '+')
     {
-       return  temp_num2 + temp_num1;
+        return  temp_num2 + temp_num1;
     }
 
     else if (ch == '-')
     {
-       return  temp_num2 - temp_num1;
+        return  temp_num2 - temp_num1;
     }
 
     else if (ch == '*')
     {
-       return  temp_num2 * temp_num1;
+        return  temp_num2 * temp_num1;
     }
 
     else if (ch == '/')
     {
-       return  temp_num2/temp_num1;
+        return  temp_num2/temp_num1;
     }
     else
     {
@@ -271,10 +279,10 @@ double add_nums (stack <string> &result,char ch)
 
 double eval_func(stack <string> &result,char ch)
 {
-    
+
     double temp_num1 = stod(result.top());
     result.pop();
-    
+
     cout<<"WORKS add_NUMS 3 "<< temp_num1<<endl;
     if (ch == 's')
     {
@@ -299,9 +307,9 @@ void save_var (string var_name,double var_num)
 
     if(it == vars.end())//check if varialbe is already in hashmap if it's not add it else update the value
     {
-    
+
         vars.insert({var_name,var_num});
-    
+
     }
     else
     {
@@ -319,12 +327,12 @@ double get_var(string var_name)
     {
         cout<<"error"<<endl;
         return -1;
-    
+
     }
     else//retrieve the value of the varialbe if it's in the map
     {
         return it->second;
-    
+
     }
 }
 
@@ -335,9 +343,9 @@ void check_var(string expr)//Take in expression and determine if it's setting a 
     string val;//used ,to check if string is "let" or just a variable name or sin cos etc;
 
     int i = 0;
-   
+
     cout<<val<<"       "<<endl;
-    
+
     val = get_word(i,expr);
     if (val == "let") //Get the variable name
     {
@@ -355,27 +363,27 @@ void check_var(string expr)//Take in expression and determine if it's setting a 
             else if(isalpha(expr[i]))
             {
                 var_name += expr[i];
-        
+
 
                 cout<<"works 3"<<endl;
             }
-        
-        cout<<"works 6"<<endl;
+
+            cout<<"works 6"<<endl;
         }   
-        
-        
+
+
         cout<<"works 8"<<endl;
         //create a substring with the expression to be saved in the variable name
         cout<<var_name<<" "<<endl;
         save_var(var_name,shantin_yard(expr.substr(i+2))); 
-        
+
     }
     else
     {
 
         cout<<"works 9"<<endl;
         shantin_yard(expr); 
-    
+
     }
 
 }
@@ -385,32 +393,32 @@ string get_word(int &i, string &expr )
     string val = "";
 
     bool word_found = false;
-    
+
     for(; i<expr.size();i++)
     {
         //If we already found the first word the break from the loop since we are only interested in checking if it's a let keyword
         cout<<"works 1"<<endl;
         if (word_found == true && !isalpha(expr[i]) ) //make sure that we are not still grabing the first word
         {
-        
+
             break;
         }
-        
+
         else if(expr[i]==' ')
         {
 
-        cout<<"works 54"<<endl;
-        
+            cout<<"works 54"<<endl;
+
         }    
-        
+
         else if(isalpha(expr[i]))
         {
             word_found = true;
             val+=expr[i];
 
-        cout<<"works 3"<<endl;
+            cout<<"works 3"<<endl;
         }
-    
+
     }
     return val;
 
